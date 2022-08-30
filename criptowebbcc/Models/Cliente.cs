7a5a6e4094485e3f521ef61665770cba00728d52
Almir Rogério Camolesi <camolesi@fema.edu.br>
2022-08-30 19:40:32 -0300

    Atualização Anotações

--- criptowebbcc/Models/Cliente.cs
+++ criptowebbcc/Models/Cliente.cs
@@ -1,13 +1,32 @@
-﻿namespace criptowebbcc.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace criptowebbcc.Models
 {
     public enum Estado {RS,SC, PR, SP, RJ, ES, MG, MS, MT, TO, GO, DF, RO, AC, AM, PA, PI, PE, PB, CE, RN}
 
+    [Table("Clientes")]
     public class Cliente
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name ="ID: ")]
         public int id { get; set; }
+
+        [Required(ErrorMessage = "Campo nome é obrigatório")]
+        [Display(Name ="Nome: ")]
+        [StringLength(35)]
         public string nome { get; set; }
+
+        [StringLength(25)]
+        [Display(Name ="Cidade: ")]
         public string cidade { get; set; }
+
+        [Display(Name = "Estado: ")]
         public Estado estado { get; set; }
+
+        [Display(Name = "Idade: ")]
+        [Range(18,70, ErrorMessage = "valor entre 18..70" )]
         public int idade { get; set; }
 
         public ICollection<Conta> contas { get; set; }
--- criptowebbcc/Models/Moeda.cs
+++ criptowebbcc/Models/Moeda.cs
@@ -1,10 +1,26 @@
-﻿namespace criptowebbcc.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace criptowebbcc.Models
 {
+    [Table("Moedas")]
     public class Moeda
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "ID: ")]
         public int id { get; set; }
+
+        [Display(Name = "Descrição: ")]
+        [StringLength(40)]
         public string descricao { get; set; }
+
+        [Display(Name = "Compra: ")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
         public float compra { get; set; }
+
+        [Display(Name = "Venda: ")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
         public float venda { get; set; }
 
         public ICollection<Conta> contas { get; set; }
